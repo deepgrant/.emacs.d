1f200c5148962b97e5d556ec3ec15b6ee70b22f8
deep.grant <grant@deep.is>
2016-02-04 09:11:51 -0500

    Initial Version.

---
+++ init.el
@@ -0,0 +1,195 @@
+;; Make sure `emacs-leuven.el' is in your `load-path'.
+(add-to-list 'load-path "/Users/gmills/GIT/emacs-leuven")
+
+;; Require it as normal.
+(require 'emacs-leuven)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
+ '(my-carriage-return-face ((((class color)) (:foreground "yellow" :background "red"))) t)
+ '(my-tab-face ((((class color)) (:foreground "white" :background "linen"))) t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq tab-width 8) ; or any other preferred value
+(defvaralias 'c-basic-offset 'tab-width)
+
+
+
+; add custom font locks to all buffers and all files
+(add-hook
+ 'font-lock-mode-hook
+ (function
+  (lambda ()
+    (setq
+     font-lock-keywords
+     (append
+      font-lock-keywords
+      '(
+        ("\r" (0 'my-carriage-return-face t))
+        ("\t" (0 'my-tab-face t))
+        ))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; make characters after column 132 purple
+(setq whitespace-line-column 132)
+
+(setq whitespace-style
+      (quote (face trailing tab-mark lines-tail)))
+(add-hook 'find-file-hook 'whitespace-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; transform literal tabs into a right-pointing triangle
+(setq
+whitespace-display-mappings ;http://ergoemacs.org/emacs/whitespace-mode.html
+'(
+   ;(tab-mark 9 [9654 9] [92 9])
+   ;others substitutions...
+   ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Turn on paren matching mode.
+(show-paren-mode 1)
+
+; Switch to matching paren when pressing % with cursor on source paren.
+(global-set-key "%" 'match-paren)
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Autosave the desktop layout every 15 seconds
+(desktop-save-mode 1)
+
+(require 'desktop)
+(desktop-save-mode 1)
+(defun my-desktop-save ()
+  (interactive)
+  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
+(add-hook 'auto-save-hook 'my-desktop-save)
+
+(setq history-length 250)
+(add-to-list 'desktop-globals-to-save 'file-name-history)
+
+(setq desktop-buffers-not-to-save
+      (concat "\\("
+	      "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
+	      "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.bbdb"
+	      "\\)$"))
+(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+(add-to-list 'desktop-modes-not-to-save 'Info-mode)
+(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+(add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
+
+(desktop-read)
+(setq desktop-enable t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(c-add-style "deep-style"
+	     '("k&r"
+	       (c-tab-always-indent . t)
+	       (c-basic-offset . 8)	; Guessed value
+	       (c-offsets-alist
+		(access-label . 0)	; Guessed value
+		(block-close . 0)	; Guessed value
+		(brace-list-close . 0)	; Guessed value
+		(brace-list-entry . 0)	; Guessed value
+		(brace-list-intro . +)	; Guessed value
+		(case-label . +)	; Guessed value
+		(class-close . 0)	; Guessed value
+		(defun-block-intro . +)	; Guessed value
+		(defun-close . 0)	; Guessed value
+		(inclass . +)		; Guessed value
+		(inline-close . 0)	; Guessed value
+		(innamespace . 0)	; Guessed value
+		(label . +)		; Guessed value
+		(member-init-cont . 0)	; Guessed value
+		(member-init-intro . +)	; Guessed value
+		(namespace-close . 0)	; Guessed value
+		(statement . 0)		; Guessed value
+		(statement-block-intro . +) ; Guessed value
+		(statement-case-intro . +)  ; Guessed value
+		(topmost-intro . 0)	    ; Guessed value
+		(topmost-intro-cont . 0)    ; Guessed value
+
+		; Base Values
+		(annotation-top-cont . 0)
+		(annotation-var-cont . +)
+		(arglist-close . c-lineup-close-paren)
+		(arglist-cont c-lineup-gcc-asm-reg 0)
+		(arglist-cont-nonempty . c-lineup-arglist)
+		(arglist-intro . +)
+		(block-open . 0)
+		(brace-entry-open . 0)
+		(brace-list-open . 0)
+		(c . c-lineup-C-comments)
+		(catch-clause . 0)
+		(class-open . 0)
+		(comment-intro . c-lineup-comment)
+		(composition-close . 0)
+		(composition-open . 0)
+		(cpp-define-intro c-lineup-cpp-define +)
+		(cpp-macro . -1000)
+		(cpp-macro-cont . +)
+		(defun-open . 0)
+		(do-while-closure . 0)
+		(else-clause . 0)
+		(extern-lang-close . 0)
+		(extern-lang-open . 0)
+		(friend . 0)
+		(func-decl-cont . +)
+		(incomposition . +)
+		(inexpr-class . +)
+		(inexpr-statement . +)
+		(inextern-lang . +)
+		(inher-cont . c-lineup-multi-inher)
+		(inher-intro . +)
+		(inlambda . c-lineup-inexpr-block)
+		(inline-open . +)
+		(inmodule . +)
+		(knr-argdecl . 0)
+		(knr-argdecl-intro . +)
+		(lambda-intro-cont . +)
+		(module-close . 0)
+		(module-open . 0)
+		(namespace-open . 0)
+		(objc-method-args-cont . c-lineup-ObjC-method-args)
+		(objc-method-call-cont c-lineup-ObjC-method-call-colons c-lineup-ObjC-method-call +)
+		(objc-method-intro .
+				   [0])
+		(statement-case-open . 0)
+		(statement-cont . +)
+		(stream-op . c-lineup-streamop)
+		(string . -1000)
+		(substatement . +)
+		(substatement-label . 2)
+		(substatement-open . +)
+		(template-args-cont c-lineup-template-args +)
+		)))
+
+(defun my-c++-mode-hook ()
+  (c-set-style "deep-style")
+  (auto-fill-mode)
+  (c-toggle-auto-hungry-state 1))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+(add-hook 'c-mode-hook 'my-c++-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
